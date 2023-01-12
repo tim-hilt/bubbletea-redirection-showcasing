0299d56d1f78cf3c2a0e392dd77f2496587a640f
Tim Hilt <thilt@in-technology.eu>
2023-01-12 13:16:17 +0100

    main: adapt example code to own example

--- main.go
+++ main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/charmbracelet/bubbles/table"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
 
 var baseStyle = lipgloss.NewStyle().
-	BorderStyle(lipgloss.NormalBorder()).
-	BorderForeground(lipgloss.Color("240"))
+	BorderStyle(lipgloss.NormalBorder())
 
 type model struct {
 	table table.Model
@@ -20,30 +16,39 @@ type model struct {
 func (m model) Init() tea.Cmd { return nil }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmd tea.Cmd
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "esc":
-			if m.table.Focused() {
-				m.table.Blur()
-			} else {
-				m.table.Focus()
-			}
-		case "q", "ctrl+c":
-			return m, tea.Quit
-		case "enter":
-			return m, tea.Batch(
-				tea.Printf("Let's go to %s!", m.table.SelectedRow()[1]),
-			)
-		}
-	}
-	m.table, cmd = m.table.Update(msg)
-	return m, cmd
+	return m, tea.Quit
 }
 
 func (m model) View() string {
 	return baseStyle.Render(m.table.View()) + "\n"
+}
+
+func Table(columns []table.Column, rows []table.Row) error {
+
+	t := table.New(
+		table.WithColumns(columns),
+		table.WithRows(rows),
+		table.WithHeight(len(rows)),
+	)
+
+	s := table.DefaultStyles()
+	s.Header = s.Header.
+		BorderStyle(lipgloss.NormalBorder()).
+		BorderBottom(true)
+	s.Selected = lipgloss.NewStyle()
+	t.SetStyles(s)
+
+	m := model{t}
+	p := tea.NewProgram(m)
+	_, err := p.Run()
+
+	if err != nil {
+		return err
+	}
+
+	p.Quit()
+
+	return nil
 }
 
 func main() {
@@ -157,28 +162,5 @@ func main() {
 		{"100", "Montreal", "Canada", "4,276,526"},
 	}
 
-	t := table.New(
-		table.WithColumns(columns),
-		table.WithRows(rows),
-		table.WithFocused(true),
-		table.WithHeight(7),
-	)
-
-	s := table.DefaultStyles()
-	s.Header = s.Header.
-		BorderStyle(lipgloss.NormalBorder()).
-		BorderForeground(lipgloss.Color("240")).
-		BorderBottom(true).
-		Bold(false)
-	s.Selected = s.Selected.
-		Foreground(lipgloss.Color("229")).
-		Background(lipgloss.Color("57")).
-		Bold(false)
-	t.SetStyles(s)
-
-	m := model{t}
-	if _, err := tea.NewProgram(m).Run(); err != nil {
-		fmt.Println("Error running program:", err)
-		os.Exit(1)
-	}
+	Table(columns, rows)
 }
